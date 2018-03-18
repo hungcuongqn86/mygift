bba7c7175a0478954b3495d8fc20a8d2e87e3718
Nguyen Hung Cuong <hungcuongqn86@gmail.com>
2018-03-18 22:42:07 +0700

    css

--- Themes/Flatly/views/layouts/master.blade.php
+++ Themes/Flatly/views/layouts/master.blade.php
@@ -391,7 +391,7 @@
             </div>
         </div>
         <hr style="box-shadow: inset 0 1px 0 0 #eef1f7, inset 0 -1px 0 0 #eef1f7;">
-        <div class="container">
+        <div class="container" id="how-we-make">
             <div class="row">
                 <div class="col-md-12">
                     <div class="how-we-make-header">
@@ -420,6 +420,110 @@
                 <div class="col-md-7">
                     <img src="http://localhost/themes/flatly/img/sd1.png"
                          alt="img">
+                </div>
+            </div>
+        </div>
+        <div style="background-color: #fff;box-shadow: inset 0 1px 0 0 #eef1f7, inset 0 -1px 0 0 #eef1f7;">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="how-we-make-header">
+                            <h3>How you get benefit from it?</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="mu-why-us-content">
+                            <div class="mu-why-us-margin">
+                                <div class="mu-why-us-oval">
+                                    <img src="http://localhost/themes/flatly/img/idea-3125-08322-e-54-d-278-4453-a-0-e-4-8-fc-62-f-4-b-501-e.png"
+                                         srcset="http://localhost/themes/flatly/img/idea-3125-08322-e-54-d-278-4453-a-0-e-4-8-fc-62-f-4-b-501-e@2x.png 2x,
+                                                        http://localhost/themes/flatly/img/idea-3125-08322-e-54-d-278-4453-a-0-e-4-8-fc-62-f-4-b-501-e@3x.png 3x"
+                                         alt="img">
+                                </div>
+                                <h3>Understand how visitor behaviour leads to sales and conversions</h3>
+                                <p>Your site is dynamic, and your conversion goals should be too. With Our solution you
+                                    can track sales, downloads, video plays, conversions, site engagement or define your
+                                    own metrics. You can also tailor your reporting based on what you want to achieve
+                                    and identify what visitor actions are most likely to help you meet your business
+                                    objectives.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="mu-why-us-content">
+                            <div class="mu-why-us-margin">
+                                <div class="mu-why-us-oval">
+                                    <img src="http://localhost/themes/flatly/img/statistics-2050-a-8-d-68351-72-f-6-407-c-9-e-67-0187-e-3389397.png"
+                                         srcset="http://localhost/themes/flatly/img/statistics-2050-a-8-d-68351-72-f-6-407-c-9-e-67-0187-e-3389397@2x.png 2x,
+                                                        http://localhost/themes/flatly/img/statistics-2050-a-8-d-68351-72-f-6-407-c-9-e-67-0187-e-3389397@3x.png 3x"
+                                         alt="img">
+                                </div>
+                                <h3>Improve your online sales with E-commerce Reporting</h3>
+                                <p>Identify your best-selling products and most valuable promotions. For complex
+                                    transactions or simple one-click purchases, E-commerce Reporting helps you
+                                    understand why customers purchase and the types of purchases that they make. You can
+                                    trace transactions to campaigns and keywords, understand shoppers' on-site behaviour
+                                    and adjust your shopping basket to increase sales and win customer loyalty.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="mu-why-us-content">
+                            <div class="mu-why-us-margin">
+                                <div class="mu-why-us-oval">
+                                    <img src="http://localhost/themes/flatly/img/layers-1687-367-f-10-c-0-4-e-39-42-b-3-9-e-55-c-9-f-9205-c-6175.png"
+                                         srcset="http://localhost/themes/flatly/img/layers-1687-367-f-10-c-0-4-e-39-42-b-3-9-e-55-c-9-f-9205-c-6175@2x.png 2x,
+                                                        http://localhost/themes/flatly/img/layers-1687-367-f-10-c-0-4-e-39-42-b-3-9-e-55-c-9-f-9205-c-6175@3x.png 3x"
+                                         alt="img">
+                                </div>
+                                <h3>See the complete picture of campaign performance with Multi-Channel Funnels</h3>
+                                <p>Don't limit your campaign measurement to the last click before a sale. With
+                                    Multi-Channel Funnels, you'll see the impact of all your digital marketing
+                                    activities, including search, display, social, affiliates, email and more. Learn
+                                    which channels introduce visitors to your products and which win sales, so that you
+                                    can better balance your marketing programmes.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="mu-why-us-content">
+                            <div class="mu-why-us-margin">
+                                <div class="mu-why-us-oval">
+                                    <img src="http://localhost/themes/flatly/img/visible-1646-575-f-69-ae-f-2-dd-43-c-2-b-00-f-9-b-65-c-116-deef.png"
+                                         srcset="http://localhost/themes/flatly/img/visible-1646-575-f-69-ae-f-2-dd-43-c-2-b-00-f-9-b-65-c-116-deef@2x.png 2x,
+                                                        http://localhost/themes/flatly/img/visible-1646-575-f-69-ae-f-2-dd-43-c-2-b-00-f-9-b-65-c-116-deef@3x.png 3x"
+                                         alt="img">
+                                </div>
+                                <h3>Follow the different paths that visitors take on your site</h3>
+                                <p>See why visitors love your site... or why they don't. Using Google Analytics Flow
+                                    Visualisation and Goal Funnels, you can follow your visitors' conversion paths and
+                                    see where they enter, where they get stuck and where they leave. Discover strengths
+                                    and weaknesses in your site navigation, and learn which areas of your website and
+                                    your marketing programme need adjustment.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="mu-why-us-content">
+                            <div class="mu-why-us-margin">
+                                <div class="mu-why-us-oval">
+                                    <img src="http://localhost/themes/flatly/img/business-report-2684-4-fa-7-fcfd-0-c-83-47-d-5-88-ca-8291-e-198-b-6-ed.png"
+                                         srcset="http://localhost/themes/flatly/img/business-report-2684-4-fa-7-fcfd-0-c-83-47-d-5-88-ca-8291-e-198-b-6-ed@2x.png 2x,
+                                                        http://localhost/themes/flatly/img/business-report-2684-4-fa-7-fcfd-0-c-83-47-d-5-88-ca-8291-e-198-b-6-ed@3x.png 3x"
+                                         alt="img">
+                                </div>
+                                <h3>Make better marketing decisions with Attribution Modelling</h3>
+                                <p>With Attribution Modelling, you can rapidly build and customise marketing attribution
+                                    models. Give credit to all of the digital marketing interactions that affected sales
+                                    or conversions. Gauge the impact of different channels, referral sources, campaigns
+                                    and keywords - so that you can improve your future marketing activities.</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
